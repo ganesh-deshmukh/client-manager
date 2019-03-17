45ecaa35a9adb4aa342324102195f124ecd75e8d
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2019-03-17 18:05:27 +0530

    added navbarComponent with branding.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppNavbar from "./components/layouts/AppNavbar";
 
 class App extends Component {
   render() {
     console.log("testing");
 
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1>App.jss</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
