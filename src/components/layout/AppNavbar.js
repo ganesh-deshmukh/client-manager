ce6beca9fa930b1aef850e2cca703eee1ae2f7de
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2019-04-02 19:34:15 +0530

    renamed dir layouts, removed warning of child in table section

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 class AppNavbar extends Component {
   render() {
-    console.log("testing");
-
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
         <div className="container">
